56ad130b69736dfeb8c6ab18bb5335e3df18f604
yjchaos <522899547@qq.com>
2016-03-08 18:22:34 +0800

    test NHiberate
    test NHiberate

---
+++ TaidouDatabase/Manager/TestUserManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaidouDatabase.Model;
+
+namespace TaidouDatabase.Manager
+{
+    class TestUserManager
+    {
+        public IList<TestUser> GetAllUser()
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var userlist = session.QueryOver<TestUser>();
+                    return userlist.List();
+                }
+            }
+        }
+
+        public IList<TestUser> GetUserByUserName(string username)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var userlist = session.QueryOver<TestUser>().Where( user => user.Username == username);
+                    return userlist.List();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            TestUserManager testUserManager = new TestUserManager();
+            IList<TestUser> testuserList = testUserManager.GetAllUser();
+            foreach (var testUser in testuserList)
+            {
+                Console.WriteLine("username = " + testUser.Username);
+            }
+            Console.WriteLine("-----------------------------------------");
+            IList<TestUser> testuserList2 = testUserManager.GetUserByUserName("yaojun");
+            foreach (var testUser in testuserList2)
+            {
+                Console.WriteLine("username = " + testUser.Username);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TaidouDatabase/Model/Mapping/TestUserMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+
+namespace TaidouDatabase.Model.Mapping
+{
+    class TestUserMap:ClassMap<TestUser>
+    {
+        public TestUserMap()
+        {
+            //这里的x是TestUser的一个对象
+            Id(x => x.Id).Column("id");  //设置id属性为主键
+            Map(x => x.Username).Column("username");
+            Map(x => x.Password).Column("password");
+            Map(x => x.Age).Column("age");
+            Table("testuser");
+        }
+    }
+}
